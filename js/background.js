92c16e4bd3edd63e368fa0ca17e93411ed347e1e
sijmenhuizenga <sijmenhuizenga@gmail.com>
2018-05-26 21:46:19 +0200

    add background ding

--- js/background.js
+++ js/background.js
@@ -1,3 +1,7 @@
-function renderBackground(){
-    console.log(level)
+function renderBackground(bitmap){
+    level.tileset.forEach(function(tile){
+        if(tile.type === "house"){
+            bitmap.draw("house_red", tile.x, tile.y, null, null, null, null, null, null, tile.rotation)
+        }
+    });
 }
