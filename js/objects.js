58846ce7200c482b9b3fb59195224a7e36ab1378
davidot <david.tuin@gmail.com>
2018-05-27 00:39:04 +0200

    Fix movement with keys
    Add basic ray casting for cameras

--- js/objects.js
+++ js/objects.js
@@ -1,6 +1,16 @@
-function renderCams(cams){
+function renderCams(poles, cams, goals){
+
     level.objects.forEach(function(object) {
-        game.add.image(object.x, object.y, object.type, 0, cams);
+        if (object.type === "camera_pole") {
+            game.add.image(object.x, object.y, object.type, 0, poles);
+        } else if (object.type === "goal" ) {
+            var sp = game.add.sprite(object.x, object.y, 'goal', null, goals);
+            game.physics.enable(sp, Phaser.Physics.ARCADE);
+            // sp.body.setSize(object.width, object.height);
+            sp.body.immovable = true;
+        } else {
+            game.add.image(object.x, object.y, object.type, 0, cams);
+        }
     });
 
     level.tiles.forEach(function(object) {
