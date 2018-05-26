baa0c7c9571e2aeb424421a57c9680d479755e59
Bram Winter <mail@bramwinter.nl>
2018-05-27 00:31:54 +0200

    Flags will be added at the edges of the map

--- js/objects.js
+++ js/objects.js
@@ -3,4 +3,12 @@ function renderCams(cams){
         game.add.image(object.x, object.y, object.type, 0, cams);
     });
 
+    level.tiles.forEach(function(object) {
+        if (object.type === "street") {
+            if (   object.x === 0 || object.x*128 === this.world.width
+                || object.y === 0 || object.y*128 === this.world.height) {
+                game.add.image(object.x*128, object.y*128, "goal", 0, cams);
+            }
+        }
+    });
 }
